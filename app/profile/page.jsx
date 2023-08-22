cfeebbd39731345877bf007b9dd35e02fdcf26cd
Kenneth Reforma <kreforma@myseneca.ca>
2023-08-22 14:08:21 -0400

    version1

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,7 +27,21 @@ const MyProfile = () => {
 		router.push(`/update-prompt?id=${post._id}`);
 	};
 	const handleDelete = async (post) => {
-		router.push(`/update-prompt?id=${post._id}`);
+		const hasConfirmed = confirm(
+			"Are you sure you want to delete this prompt?"
+		);
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+				const filteredPosts = posts.filter((p) => {
+					p._id !== post._id;
+				});
+
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error);
+			}
+		}
 	};
 	return (
 		<Profile
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -55,7 +55,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 				className="font-inter text-sm blue_gradient cursor-pointer"
 				onClick={() => handleTagClick && handleTagClick(post.tag)}
 			>
-				{post.tag}
+				#{post.tag}
 			</p>
 			{session?.user.id === post.creator._id && pathName === "/profile" && (
 				<div className="mt-5 flex-center gap-4 border-t border-gray-100">
