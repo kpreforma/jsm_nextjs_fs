b97edaedd45e9339b744fbe9f8e373f4fc775077
Kenneth Reforma <kreforma@myseneca.ca>
2023-08-21 23:36:00 -0400

    auth function done with google SSO

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
 	title: "Promptopia",
@@ -9,10 +11,16 @@ const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
 			<body>
-				<div className="main">
-					<div className="gradient" />
-				</div>
-				<main className="app">{children}</main>
+				<Provider>
+					<div className="main">
+						<div className="gradient" />
+					</div>
+
+					<main className="app">
+						<Nav />
+						{children}
+					</main>
+				</Provider>
 			</body>
 		</html>
 	);
